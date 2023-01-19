3edce2427136b7c045103bd1f04b5ce2922fa613
Felipe Alves <profelipealves@outlook.com>
2023-01-19 17:30:14 -0300

    add Dialog

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,35 @@
-import { Plus } from 'phosphor-react';
+import { Plus, X } from "phosphor-react";
 import logoImg from "../assets/logo.svg";
+import * as Dialog from "@radix-ui/react-dialog";
 
 export function Header() {
-    return (
-      <div className="w-full max-w-3xl mx-auto flex items-center justify-between">
+  return (
+    <div className="w-full max-w-3xl mx-auto flex items-center justify-between">
       <img src={logoImg} alt="Habit Tracker" />
-      <button 
-      className="border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300" 
-      type="button"
-      >
-        <Plus size={20} className='text-violet-500' />
-        Adicionar hábito
-      </button>
+
+      <Dialog.Root>
+        <Dialog.Trigger
+          onClick={buttonClicked}
+          className="border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300"
+          type="button"
+        >
+          <Plus size={20} className="text-violet-500" />
+          Adicionar hábito
+        </Dialog.Trigger>
+
+        <Dialog.Portal>
+          <Dialog.Overlay className="fixed inset-0 bg-black/80 w-screen h-screen" />
+          <Dialog.Content
+            className="absolute p-10 bg-zinc-900 rounded-2xl w-full max-w-md top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+          >
+            <Dialog.Close className="absolute top-6 right-6 text-zinc-400 hover:text-zinc-200">
+              <X size={24} arial-label='Fechar' />
+            </Dialog.Close>
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
     </div>
-    );
+  );
 }
+
+//parei em 56:00
--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -25,16 +25,20 @@ export function SummaryTable() {
       </div>
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
-        {summaryDates.map(date => {
+        {summaryDates.map((date) => {
           return <HabitDay key={date.toString()} />;
         })}
 
-        {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, i) => {
-          return (
-             <div key={i} className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed" />;
-          )
-        })}
+        {amountOfDaysToFill > 0 &&
+          Array.from({ length: amountOfDaysToFill }).map((_, i) => {
+            return (
+              <div
+                key={i}
+                className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
+              />
+            );
+          })}
       </div>
     </div>
   );
-      }
+}
