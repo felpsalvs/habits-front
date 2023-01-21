5ced97aa68236ace0c62222fa2e8acc8656c281e
Felipe Alves <profelipealves@outlook.com>
2023-01-21 09:24:40 -0300

    :lipstick: update the UI and style files.

--- src/components/NewHabitForm.tsx
+++ src/components/NewHabitForm.tsx
@@ -21,20 +21,21 @@ export function NewHabitForm() {
       </label>
 
       <div className="flex flex-col gap-2 mt-3">
-      <Checkbox.Root className="flex items-center gap-3 group">
-              <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
-                <Checkbox.Indicator>
-                  <Check size={20} className="text-white" />
-                </Checkbox.Indicator>
-              </div>
+        <Checkbox.Root className="flex items-center gap-3 group">
+          <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+            <Checkbox.Indicator>
+              <Check size={20} className="text-white" />
+            </Checkbox.Indicator>
+          </div>
 
-              <span className="text-white font-semibold text-xl leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400">
-                Beber 2l de água
-              </span>
-            </Checkbox.Root>
+          <span className="text-white leading-tight">Segunda-feira</span>
+        </Checkbox.Root>
       </div>
 
-      <button type="submit" className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500">
+      <button
+        type="submit"
+        className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500"
+      >
         <Check size={20} weight="bold" />
         Confirmar
       </button>
