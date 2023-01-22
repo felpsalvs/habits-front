83f3a49d5633e005757775bd86c567aef9209604
Felipe Alves <profelipealves@outlook.com>
2023-01-22 09:31:39 -0300

    :bug: fix error

--- src/components/HabitsList.tsx
+++ src/components/HabitsList.tsx
@@ -43,7 +43,7 @@ export function HabitsList({ date, onCompletedChanged }: HabitsListProps) {
 
     if (isHabitAlreadyCompleted) {
       completedHabits = habitsInfo!.completedHabits.filter(
-        (id) => (id /= habitId)
+        (id) => id !== habitId
       );
     } else {
       completedHabits = [...habitsInfo!.completedHabits, habitId];
