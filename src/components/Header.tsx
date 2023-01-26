4740e3714a7e1ebb7ce84ce536322310c8971c6f
Felipe Alves <profelipealves@outlook.com>
2023-01-26 17:58:29 -0300

    feat: update format code

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,19 +2,21 @@ import { Plus, X } from "phosphor-react";
 import logoImg from "../assets/logo.svg";
 import * as Dialog from "@radix-ui/react-dialog";
 import { NewHabitForm } from "./NewHabitForm";
+import { ToastComponent } from "./Toast";
 
-export function Header() {
+export const Header = () => {
   return (
     <div className="w-full max-w-3xl mx-auto flex items-center justify-between">
-      <img src={logoImg} alt="Habit Tracker" />
+    <ToastComponent />  
+    <img src={logoImg} alt="Habit Tracker" />
 
       <Dialog.Root>
         <Dialog.Trigger
           className="border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-violet-600 focus:ring-offset-2 focus:ring-offset-background"
           type="button"
         >
-          <Plus size={20} className="text-violet-500" />
-          Adicionar hábito
+          <Plus className="text-white" />
+          Adicionar novo hábito
         </Dialog.Trigger>
 
         <Dialog.Portal>
@@ -23,7 +25,7 @@ export function Header() {
             <Dialog.Close className="absolute right-6 top-6 text-zinc-400 rounded-lg hover:text-zinc-200 focus:outline-none focus:ring-2 focus:ring-violet-600 focus:ring-offset-2 focus:ring-offset-zinc-900">
               <X size={24} arial-label="Fechar" />
             </Dialog.Close>
-            <Dialog.Title className="text-3xl leading-tight font-extrabold">
+            <Dialog.Title className="text-3xl text-zinc-300 leading-tight font-extrabold">
               Adicionar hábito
             </Dialog.Title>
             <NewHabitForm />
